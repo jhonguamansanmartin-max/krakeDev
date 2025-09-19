6ff5f463b40282af1a7440deac9b47d724d4a8da
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2025-09-18 23:50:17 -0500

    incremento la funcion float y int

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -10,5 +10,21 @@ saludar=function(){
     //recuperar el valor de la caja de texto 
     let nombre=recuperartexto("txtnombre");
     //recuperar el valor de la caja apellido 
-    let apellido=recuperartexto("apellido")
+    let apellido=recuperartexto("apellido");
+
+   let edad=recuperarint("txtedad");
+
+   let estatura=recuperarfloat("txtestatura");
+}
+recuperarint=function(idComponente){
+    let valorcaja=recuperartexto(idComponente);
+    let valorentero=parseInt(valorcaja);
+    return valorentero;
+
+}
+recuperarfloat=function(idComponente){
+    let valorcaja= recuperartexto(idComponente);
+    let valorflotante= parseFloat(valorcaja);
+    return valorflotante;
+
 }

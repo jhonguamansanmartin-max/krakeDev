3615419845495a532276a1c07cf9274c15ba23bf
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2025-10-14 22:15:45 -0500

    funcion mostrarnota valida

--- 2-calculadora/notas.js
+++ 2-calculadora/notas.js
@@ -36,7 +36,7 @@ calcular1 = function () {
     mostrarTexto("lblResultado", resultadoFormato);
 }
 
-calcular = function () {
+calcular= function () {
     let nota1;
     let nota2;
     let nota3;
@@ -45,33 +45,34 @@ calcular = function () {
     let existeError = false
 
     nota1 = recuperarFloat("txtNota1");
-    if (isNaN(nota1)) {
-        mostrarTexto("lblError1", "Debe ingresar un numero")
-        existeError = true;
-    } else {
-        mostrarTexto("lblError1", "")
-    }
+            esnotavalida(nota1, "lblError1")
+
     nota2 = recuperarFloat("txtNota2");
-    if (isNaN(nota2)) {
-        mostrarTexto("lblError2", "Debe ingresar un numero")
-        existeError = true;
-    } else {
-        mostrarTexto("lblError2", "")
-    }
+             esnotavalida(nota2, "lblError2")
 
     nota3 = recuperarFloat("txtNota3");
-    if (isNaN(nota3)) {
-        mostrarTexto("lblError3", "Debe ingresar un numero")
-        existeError = true;
-    } else {
-        mostrarTexto("lblError3", "")
-    }
-    if (existeError == false) {
-        resultado = calcularPromedio(nota1, nota2, nota3);
+            esnotavalida(nota3, "lblError3")
+
+    if( esnotavalida(nota1, "lblError1") && esnotavalida(nota2, "lblError2") &&  esnotavalida(nota3, "lblError3") ){
+         resultado = calcularPromedio(nota1, nota2, nota3);
         resultadoFormato = resultado.toFixed(2);
         mostrarTexto("lblResultado", resultadoFormato);
+    }else{
+        mostrarTexto("lblResultado" , "0.0")
+    }
+   
+}
+ esnotavalida=function(nota,idcomponenteError){
+    
+    if (isNaN(nota)) {
+        mostrarTexto(idcomponenteError, "Debe ingresar un numero")
+        existeError = true;
+        return false;
+    } else {
+        mostrarTexto(idcomponenteError, "")
+        return true;
     }
 
-}
+ }
 
 

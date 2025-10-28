6c490c689b9ffa47663d599c1469e0d37d4790f7
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2025-10-27 23:28:59 -0500

    tabla de mostrar empleados implementada

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,16 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo: " $ " + 500.0},
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:" $ "+ 900.0},
+    {cedula:"45556897465",nombre:"Pedro",apellido:"larrea",sueldo:" $ " + 450.0}
 ]
 
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol")
     ocultarComponente("divResumen")
+
+    mostrarEmpleado()
+
    }
 mostrarOpcionRol=function(){
     mostrarComponente("divRol")
@@ -17,4 +21,25 @@ mostrarOpcionResumen=function(){
     mostrarComponente("divResumen")
     ocultarComponente("divEmpleado")
     ocultarComponente("divRol")
+}
+mostrarEmpleado=function(){
+    let elemntoTabla = document.getElementById("tablaEmpleados")
+    let contenidoTabla = "<table><tr>" +
+        "<th>Cedula</th>" +
+        "<th>Nombre</th>" +
+        "<th>Apellido</th>"+
+        "<th>Sueldo</th>"+
+    "</tr>";
+    let elementoEmpleado;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i];
+        contenidoTabla +=
+            "<tr><td>" + elementoEmpleado.cedula + "</td>"
+            + "<td>" + elementoEmpleado.nombre + "</td>"
+            + "<td>" + elementoEmpleado.apellido + "</td>"
+            + "<td>" + elementoEmpleado.sueldo + "</td>"
+            + "</tr>"
+    }
+    contenidoTabla += "</table>"
+    elemntoTabla.innerHTML= contenidoTabla ;
 }
